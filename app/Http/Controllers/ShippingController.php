31da1b94cd16d5d89e0782217e9fafef83d96264
Anastasiia Stepanova <nastyaaa19980@gmail.com>
2023-04-12 07:43:27 +0300

    Add api endpoint for shipping, edit ShippingController method

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -5,20 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Shipping\ShippingRequest;
 use App\Services\Shipping\ShippingService;
 use ErrorException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 
 class ShippingController extends Controller
 {
     /**
      * @param ShippingRequest $request
-     * @return RedirectResponse
+     * @return RedirectResponse|JsonResponse
      * @throws ErrorException
      */
-    public function shipping(ShippingRequest $request): RedirectResponse
+    public function shipping(ShippingRequest $request): RedirectResponse|JsonResponse
     {
         $validated = $request->validated();
         $result = ShippingService::makeShipping($validated);
 
-        return redirect('/')->with('order', $result);
+        if ($request->expectsJson()) {
+            return response()->json($result);
+        } else {
+            return redirect('/')->with('order', $result);
+        }
     }
 }
